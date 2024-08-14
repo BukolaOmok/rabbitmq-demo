6c384ceca5cea6788241f80005dc1c24bf47e786
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-08-14 15:03:27 +0100

    change message and queue name

--- src/promise-api-2/send.js
+++ src/promise-api-2/send.js
@@ -5,7 +5,7 @@ async function doSendingDemo() {
     const exchangeURL = getEnvironmentVariableOrFail("AMQP_EXCHANGE_URL");
     const conn = await connect(exchangeURL);
 
-    const queueName = "neill-tasks";
+    const queueName = "bukola-tasks";
     const channel = await conn.createChannel();
     //will only create the queue if it doesn't already exist
     await channel.assertQueue(queueName, { durable: false });
@@ -28,8 +28,8 @@ doSendingDemo();
 
 function makeMessage() {
     const time = performance.now();
-    const playerName = "MysteryScholar";
-    const hitPoints = 100;
+    const playerName = "Bukola";
+    const hitPoints = 500;
     const data = { playerName, time, hitPoints };
     return JSON.stringify(data);
 }
